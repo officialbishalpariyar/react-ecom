46ddfb54dd29073bba3d0b08122eb14b6f3c4318
unknown <officialbishalpariyar@gmail.com>
2022-11-03 16:59:53 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import About from "./About";
+import Home from "./Home";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  const theme ={
+    colors:{
+      bg: "#fff",
+    },
+  };
+  return 
+  // <ThemeProvider theme={theme}>
+  <BrowserRouter>
+  {/* <GlobalStyle/> */}
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />}/>
+    <Route path="/products" element={<Products />}/>
+    <Route path="/contact" element={<Contact />}/>
+    <Route path="/singleproduct/:id" element={<SingleProduct />}/>
+    <Route path="/cart" element={<Cart />}/>
+    <Route path="*" element={<ErrorPage />}/>
+    </Routes>
+  </BrowserRouter>
+  // </ThemeProvider>
+  ;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<React.StrictMode><App /></React.StrictMode>);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
